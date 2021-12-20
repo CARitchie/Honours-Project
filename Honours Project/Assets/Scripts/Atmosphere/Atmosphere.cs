cf948faad6301265c70efdb884f0dcdaf1b86ff8
Defender <craig.ritchie450@gmail.com>
2021-12-20 14:15:29 +0000

    Created a more realistic atmosphere shader

--- Honours Project/Assets/Scripts/Atmosphere/Atmosphere.cs
+++ Honours Project/Assets/Scripts/Atmosphere/Atmosphere.cs
@@ -6,9 +6,11 @@ public class Atmosphere : MonoBehaviour
 {
     [SerializeField] float planetRadius;
     [SerializeField] float atmosphereRadius;
-    [Range(1,500)] [SerializeField] int numberOfSteps;
+    [Range(2,100)] [SerializeField] int numberOfSteps;
     [SerializeField] float strength;
     [SerializeField] float sunsetStrength;
+    [SerializeField] Vector3 wavelengths = new Vector3(700,530,440);
+    [SerializeField] float scatterStrength = 1;
 
     public Material ModifyMaterial(Material mat)
     {
@@ -18,6 +20,13 @@ public class Atmosphere : MonoBehaviour
         mat.SetInt("_NumberOfSteps", numberOfSteps);
         mat.SetFloat("_Strength", strength);
         mat.SetFloat("_SunsetStrength", sunsetStrength);
+
+        float scatterR = Mathf.Pow(400 / wavelengths.x,4) * scatterStrength;
+        float scatterG = Mathf.Pow(400 / wavelengths.y,4) * scatterStrength;
+        float scatterB = Mathf.Pow(400 / wavelengths.z,4) * scatterStrength;
+        Vector3 scatteringCoefficients = new Vector3(scatterR,scatterG,scatterB);
+        mat.SetVector("scatteringCoefficients",scatteringCoefficients);
+
         return mat;
     }
 }
