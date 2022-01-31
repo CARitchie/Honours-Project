19c57c7efaf7313f88d8b961be9563822b1099dc
Craig <craig.ritchie450@gmail.com>
2022-01-31 14:36:31 +0000

    Created tool to randomly place props on a planet

---
+++ Honours Project/Assets/Scripts/Tools/Editor/PropScheme.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "My Assets/Prop Placer/Prop Scheme")]
+public class PropScheme : ScriptableObject
+{
+    [SerializeField] PlaceableProp[] props;
+    float sumOfWeights;
+
+    public void CalculateWeights()
+    {
+        sumOfWeights = 0;
+        foreach(PlaceableProp prop in props)
+        {
+            sumOfWeights += prop.GetWeight();
+        }
+    }
+
+    public PlaceableProp GetRandomProp()
+    {
+        float bigWeight = Random.Range(0, sumOfWeights);
+        for(int i = 0; i < props.Length; i++)
+        {
+            float weight = props[i].GetWeight();
+            if (bigWeight < weight) return props[i];
+            bigWeight -= weight;
+        }
+
+        return null;
+    }
+
+}
